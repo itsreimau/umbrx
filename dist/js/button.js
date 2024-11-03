3104d31f595f260e2d98b4a7b39734c4cd7f963a
itsreimau <private.iblmln@gmail.com>
2024-11-03 16:00:05 +0700

    [Update] updated icon and changed WhatsApp links, improved responsive design for smaller screens, and adjusted CSS styles.

--- dist/js/button.js
+++ dist/js/button.js
@@ -1,7 +1,7 @@
-document.getElementById("tryBtn").addEventListener("click", function () {
+document.getElementById("tryBtn").addEventListener("click", function() {
     window.open("https://wa.me/6281918403548", "_blank");
 });
 
-document.getElementById("supportBtn").addEventListener("click", function () {
+document.getElementById("supportBtn").addEventListener("click", function() {
     window.open("https://chat.whatsapp.com/FlqTGm4chSjKMsijcqAIJs", "_blank");
 });
