b79a84996012c905898edc11722093187c305d18
itsreimau <private.iblmln@gmail.com>
2025-01-12 16:35:48 +0700

    [Remove] Removed old styles, icons, and logos.

--- dist/js/button.js
+++ dist/js/button.js
@@ -1,7 +1,31 @@
-document.getElementById("tryBtn").addEventListener("click", function() {
+// UmbrX
+document.getElementById("btn-try-umbrx").addEventListener("click", function() {
     window.open("https://wa.me/6288293456464", "_blank");
 });
+document.getElementById("btn-support-umbrx").addEventListener("click", function() {
+    window.open("https://chat.whatsapp.com/FxEYZl2UyzAEI2yhaH34Ye", "_blank");
+});
 
-document.getElementById("supportBtn").addEventListener("click", function() {
+// Kirito Assalam
+document.getElementById("btn-try-krtsslm").addEventListener("click", function() {
+    window.open("https://wa.me/6283845066757", "_blank");
+});
+document.getElementById("btn-support-krtsslm").addEventListener("click", function() {
+    window.open("https://chat.whatsapp.com/IMXSad7rOtc80BH5hkbWPe", "_blank");
+});
+
+// Akselia Yourbae
+document.getElementById("btn-try-kslyrb").addEventListener("click", function() {
+    window.open("https://wa.me/6288271525726", "_blank");
+});
+document.getElementById("btn-support-kslyrb").addEventListener("click", function() {
+    window.open("https://chat.whatsapp.com/FxEYZl2UyzAEI2yhaH34Ye", "_blank");
+});
+
+// UmbrX
+document.getElementById("btn-try-umbrx").addEventListener("click", function() {
+    window.open("https://wa.me/6288293456464", "_blank");
+});
+document.getElementById("btn-support-umbrx").addEventListener("click", function() {
     window.open("https://chat.whatsapp.com/FxEYZl2UyzAEI2yhaH34Ye", "_blank");
 });
