cfdff1361aaf21d34729c035054ca86ac78ed8b9
itsreimau <private.iblmln@gmail.com>
2025-01-27 15:52:57 +0700

    [Update] Corrected typo errors and updated background images in HTML files.

--- dist/js/script.js
+++ dist/js/script.js
@@ -28,33 +28,33 @@ document.getElementById("btn-support-umbrx").addEventListener("click", () => {
 });
 
 // Button actions for "Kirito Assalam"
-document.getElementById("btn-try-krtsslm").addEventListener("click", function() {
+document.getElementById("btn-try-krtsslm").addEventListener("click", function () {
     window.open("https://wa.me/6283845066757", "_blank");
 });
-document.getElementById("btn-support-krtsslm").addEventListener("click", function() {
+document.getElementById("btn-support-krtsslm").addEventListener("click", function () {
     window.open("https://chat.whatsapp.com/IMXSad7rOtc80BH5hkbWPe", "_blank");
 });
 
 // Button actions for "Akselia Yourbae"
-document.getElementById("btn-try-kslyrb").addEventListener("click", function() {
+document.getElementById("btn-try-kslyrb").addEventListener("click", function () {
     window.open("https://wa.me/6288271525726", "_blank");
 });
-document.getElementById("btn-support-kslyrb").addEventListener("click", function() {
+document.getElementById("btn-support-kslyrb").addEventListener("click", function () {
     window.open("https://chat.whatsapp.com/FxEYZl2UyzAEI2yhaH34Ye", "_blank");
 });
 
 // Button actions for "Kamisato Ayaka"
-document.getElementById("btn-try-kmstyk").addEventListener("click", function() {
+document.getElementById("btn-try-kmstyk").addEventListener("click", function () {
     window.open("https://wa.me/6285165309861", "_blank");
 });
-document.getElementById("btn-support-kmstyk").addEventListener("click", function() {
+document.getElementById("btn-support-kmstyk").addEventListener("click", function () {
     window.open("https://chat.whatsapp.com/GtRlLAKLw9JG4ISqb09PeS", "_blank");
 });
 
 // Button actions for "Mambo"
-document.getElementById("btn-try-kmstyk").addEventListener("click", function() {
+document.getElementById("btn-try-mambo").addEventListener("click", function () {
     window.open("https://wa.me/62895400967027", "_blank");
 });
-document.getElementById("btn-support-kmstyk").addEventListener("click", function() {
+document.getElementById("btn-support-mambo").addEventListener("click", function () {
     window.open("https://chat.whatsapp.com/ES6AlKeJmZL4C1m4B0Su2t", "_blank");
 });
